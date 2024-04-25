9fc302f35b34a7d66c5fd0cdf0eb806ab1d5e7c9
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-04-25 15:51:06 +0600

    accessing list

--- List.py
+++ List.py
@@ -8,3 +8,13 @@ chars = list("Hello World")
 # print(numbers)
 # print(len(chars))
 print(combined)
+
+
+# Accessing items
+letters = ["a", "b", "c", "d"]
+letters[0] = "A"
+# print(letters)
+# print(letters[0:3])
+# print(letters[::2])
+# print(numbers[::2])
+# print(numbers[::-1])
