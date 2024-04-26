bd3d765e0c8bdb20eb20c0645091154b4d433f58
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-04-26 13:49:22 +0600

    sort-2nd

--- List-sorting.py
+++ List-sorting.py
@@ -1,6 +1,22 @@
 numbers = [3, 51, 8, 6, 4, 10]
-numbers.sort()
+# numbers.sort()
+# print(numbers)
+
+# numbers.sort(reverse=True)
+print(sorted(numbers, reverse=True))
 print(numbers)
 
-numbers.sort(reverse=True)
-print(numbers)
+
+items = [
+    ('product1', 10),
+    ('product2', 80),
+    ('product3', 40),
+]
+
+
+def sort_item(item):
+    return item[1]
+
+
+items.sort(key=sort_item)
+print(items)
