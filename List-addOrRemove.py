37d8c51ca20b2462451256f1f205bead63e8dd92
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-04-26 13:29:34 +0600

    add-remove

---
+++ List-addOrRemove.py
@@ -0,0 +1,14 @@
+letters = ["a", "b", "c"]
+
+# Add
+
+letters.append("d")
+letters.insert(0, "!")    # For Add at a specific index
+
+# Remove #
+
+# letters.pop(0)
+# letters.remove("b")
+# del letters[0:3]
+# letters.clear()
+print(letters)
