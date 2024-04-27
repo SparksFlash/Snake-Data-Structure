0421df2f70dec17088894ead61ac7572cad8f399
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-04-27 16:16:01 +0600

    map-done

--- Map-function.py
+++ Map-function.py
@@ -7,13 +7,13 @@ items = [
 # prices = []
 # for item in items:
 #     prices.append(item[1])
-
 # print(prices)
 
+# we can also achieve in this way
 x = map(lambda item: item[1], items)
 for item in x:
     print(item)
 
-
+# we can also achieve in this way also
 prices = list(map(lambda item: item[1], items))
 print(prices)
