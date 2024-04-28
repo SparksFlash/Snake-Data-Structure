b69c6d2edf857eb93a6fefa820fce4f11ad1ff23
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-04-29 02:28:34 +0600

    filtered-comprehensions

--- List-comprehensions.py
+++ List-comprehensions.py
@@ -7,3 +7,7 @@ items = [
 # prices = list(map(lambda item: item[1], items))
 prices = [item[1] for item in items]
 print(prices)
+
+# filtered = list(filter(lambda item: item[1] >= 40, items))
+filtered = [item for item in items if item[1] >= 40]
+print(filtered)
