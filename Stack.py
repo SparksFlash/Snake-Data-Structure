c7cee867c559458bca753b259161760bde5b2e48
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-05-01 23:01:51 +0600

    stack-done

--- Stack.py
+++ Stack.py
@@ -1,3 +1,5 @@
+# LIFO Behaviour
+
 browsing_session = []
 browsing_session.append(1)
 browsing_session.append(2)
@@ -6,3 +8,5 @@ print(browsing_session)
 last = browsing_session.pop()
 print(last)
 print("Redirect", browsing_session[-1])
+if not browsing_session:
+    browsing_session[-1]
