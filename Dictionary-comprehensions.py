8993659c67b8d74fc47caf84a41995263a1d0722
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-05-07 17:33:58 +0600

    for-set-format

--- Dictionary-comprehensions.py
+++ Dictionary-comprehensions.py
@@ -3,4 +3,6 @@
 #     values.append(x * 2)
 
 # In the above 3 line code is identical to -
-values = [x * 2 for x in range(5)]
+# values = [x * 2 for x in range(5)]
+values = {x * 2 for x in range(5)}
+print(values)
