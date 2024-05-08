21eaa6c5a062de0fdfc689c1cf08f37b669b0900
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-05-08 22:17:04 +0600

    getsizeof

--- Generator-expressions.py
+++ Generator-expressions.py
@@ -1,5 +1,10 @@
 # Here we use parantheses for creating generator object
+from sys import getsizeof
 values = (x * 2 for x in range(10))
 print(values)
 for x in values:
     print(x)
+
+
+test = (x * 2 for x in range(1000))
+print("Gen :", getsizeof(test))
