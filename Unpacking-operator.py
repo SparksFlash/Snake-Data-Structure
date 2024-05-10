5fa9b7505eeb8e03574d456f6b881b119b664709
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-05-10 16:31:43 +0600

    multi-list-add

--- Unpacking-operator.py
+++ Unpacking-operator.py
@@ -7,3 +7,13 @@ print(*numbers)
 values = list(range(5))
 values = [*range(5), *"Hello"]
 print(values)
+
+first = [1, 2, 3, 4]
+second = [7, 8, 9]
+result = [*first, "a", *second, *"Hello"]
+print(result)
+
+one = {"x": 1}
+two = {"y": 2, "z": 1}
+combined = {**one, **two, "z": 9}
+print(combined)
