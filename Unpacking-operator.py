aa1625389490666993b805f9efff75c18682bdf4
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-05-10 16:18:20 +0600

    commet

--- Unpacking-operator.py
+++ Unpacking-operator.py
@@ -1,5 +1,8 @@
 numbers = [1, 2, 3, 4, 5]
-print(*numbers)            # Unpacking operator
+print(*numbers)
+
+# Unpacking operator
+# Using this operator we can combined multiple list
 
 values = list(range(5))
 values = [*range(5), *"Hello"]
