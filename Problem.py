7138baf01b90fec96bd74d9e43ef2b3924e2cd7f
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-05-11 19:41:39 +0600

    problem

---
+++ Problem.py
@@ -0,0 +1,13 @@
+# Find the most repeated character in this sentence
+
+
+from pprint import pprint
+sentence = "This is a common interview question"
+
+char_frequency = {}
+for char in sentence:
+    if char in char_frequency:
+        char_frequency[char] += 1
+    else:
+        char_frequency[char] = 1
+pprint(char_frequency)
